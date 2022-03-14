d488382bdfbd3a8f01d21ed1425276d13ec7d681
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-03-14 23:17:32 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace Snake_And_Ladder_Program
         static void Main(string[] args)
         {
             int player1 = 0;
+            int rollDie1_p1 = 0;
+            Random obj = new Random();
+                int randomDie1 = obj.Next(1, 7);
+                rollDie1_p1++;
         }
     }
 }
