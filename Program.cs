9fe4ff3126f8eaf1746b7a41981b829523bf0c26
parthgarg977 <67135234+Parthgarg977@users.noreply.github.com>
2022-03-14 23:19:37 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,26 @@ namespace Snake_And_Ladder_Program
             Random obj = new Random();
                 int randomDie1 = obj.Next(1, 7);
                 rollDie1_p1++;
+            string[] names = new string[] { "no play", "ladder", "snake" };
+                Random rand = new Random();
+                int randomOption1 = rand.Next(names.Length);
+
+                if (names[randomOption1] == "ladder")
+                {
+                    player1 += randomDie1;
+                    if (player1 > 100)
+                    {
+                        player1 -= randomDie1;
+                    }
+                }
+                else if (names[randomOption1] == "snake")
+                {
+                    player1 -= randomDie1;
+                    if (player1 < 0)
+                    {
+                        player1 += randomDie1;
+                    }
+                }
         }
     }
 }
